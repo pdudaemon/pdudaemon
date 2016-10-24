7dcbc6aa36c1634d60df7adc729e90d575c9292b
Sjoerd Simons <sjoerd.simons@collabora.co.uk>
2016-10-24 23:11:45 +0200

    daemons: Add option to stay in the foreground
    Add a command line option to stay in the foreground which makes things
    more convenient for debugging/testing. When staying in the foreground
    log output gets put on stdout.

--- lavapdu/shared.py
+++ lavapdu/shared.py
@@ -4,6 +4,7 @@ import logging
 import json
 import argparse
 from logging.handlers import WatchedFileHandler
+from logging import StreamHandler
 import daemon
 try:
     import daemon.pidlockfile as pidlockfile
@@ -14,14 +15,17 @@ def get_daemon_logger(filepath, log_format=None, loglevel=logging.INFO):
     logger = logging.getLogger()
     logger.setLevel(loglevel)
     try:
-        watchedhandler = WatchedFileHandler(filepath)
+        if filepath:
+            handler = WatchedFileHandler(filepath)
+        else:
+            handler = StreamHandler()
     except Exception as e:  # pylint: disable=broad-except
         return e
 
-    watchedhandler.setFormatter(logging.Formatter(log_format
-                                                  or '%(asctime)s %(msg)s'))
-    logger.addHandler(watchedhandler)
-    return logger, watchedhandler
+    handler.setFormatter(logging.Formatter(log_format
+                                           or '%(asctime)s %(msg)s'))
+    logger.addHandler(handler)
+    return logger, handler
 
 
 def read_settings(filename):
@@ -49,10 +53,6 @@ def pdus_from_config(data):
 
 def setup_daemon(options, settings, pidfile):
     logfile = options.logfile
-    if not os.path.exists(os.path.dirname(options.logfile)):
-        print "No such directory for specified logfile '%s'" % logfile
-
-    open(logfile, 'w').close()
     level = logging.DEBUG
     daemon_settings = settings["daemon"]
     if daemon_settings["logging_level"] == "DEBUG":
@@ -63,8 +63,8 @@ def setup_daemon(options, settings, pidfile):
         level = logging.ERROR
     if daemon_settings["logging_level"] == "INFO":
         level = logging.INFO
-    client_logger, watched_file_handler = get_daemon_logger(
-        logfile,
+    client_logger, handler = get_daemon_logger(
+        None if options.foreground else logfile,
         loglevel=level,
         log_format='%(asctime)s:%(levelname)s:%(name)s:%(message)s')
     if isinstance(client_logger, Exception):
@@ -76,18 +76,21 @@ def setup_daemon(options, settings, pidfile):
         logging.error("PIDFile %s already locked" % pidfile)
         sys.exit(os.EX_OSERR)
     context = daemon.DaemonContext(
-        detach_process=True,
+        detach_process=not options.foreground,
         working_directory=os.getcwd(),
         pidfile=lockfile,
-        files_preserve=[watched_file_handler.stream],
-        stderr=watched_file_handler.stream,
-        stdout=watched_file_handler.stream)
+        files_preserve=[handler.stream],
+        stderr=handler.stream,
+        stdout=handler.stream)
 
     return context
 
 
 def get_common_argparser(description, logfile):
     parser = argparse.ArgumentParser(description=description)
+    parser.add_argument("--foreground", "-f",
+                        help="Stay in the foreground",
+                        action="store_true", default=False)
     parser.add_argument("--logfile", dest="logfile", action="store",
                         type=str, default=logfile,
                         help="log file [%s]" % logfile)
