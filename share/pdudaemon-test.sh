e5f9181f8fcb42b1a7675f350db0b8453901fdcf
Matt Hart <matthew.hart@linaro.org>
2018-05-23 12:55:32 +0100

    Add ubuntu systemd dev packages

--- share/pdudaemon-test.sh
+++ share/pdudaemon-test.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-./setup.py install
 pdudaemon --loglevel=DEBUG --conf=share/pdudaemon.conf --dbfile=/tmp/dbfile &
 sleep 5
 curl -q "http://localhost:16421/power/control/reboot?hostname=test&port=1&delay=5" &> /dev/null
