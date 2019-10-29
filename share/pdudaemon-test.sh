891bba28092fc3f4864a85e2a3f0822632780cc3
Matt Hart <matthew.hart@linaro.org>
2019-10-29 12:17:57 +0100

    Add testing for --drive mode
    Update the very basic test setup to use the --drive support, and support
    running the test script locally for development.

--- share/pdudaemon-test.sh
+++ share/pdudaemon-test.sh
@@ -1,17 +1,53 @@
 #!/bin/bash
-pdudaemon --loglevel=DEBUG --conf=share/pdudaemon.conf --dbfile=/tmp/dbfile &
-sleep 5
-curl -q "http://localhost:16421/power/control/reboot?hostname=test&port=1&delay=5" &> /dev/null
-sleep 15
-LINES=`cat /tmp/pdu | wc -l`
-if [[ $LINES -eq 2 ]]
+
+PDUD_BINARY=pdudaemon
+TMPFILE=/tmp/pdu
+
+while getopts l option
+do
+  case "${option}" in
+    l)
+      LOCAL=true
+      ;;
+  esac
+done
+
+#empty the tempfile, helpful if running locally
+if [ $LOCAL ]
 then
-  echo "localcmdline executed ok"
-  cat /tmp/pdu
-  exit 0
-else
-  echo "localcmdline test failed"
-  cat /tmp/pdu
-  exit 1
+  cp pdudaemon/__init__.py ./pdudaemon-test-bin
+  chmod +x ./pdudaemon-test-bin
+  PDUD_BINARY=./pdudaemon-test-bin
+  echo -n "" > $TMPFILE
 fi
 
+$PDUD_BINARY --loglevel=DEBUG --conf=share/pdudaemon.conf --dbfile=/tmp/dbfile &
+PDU_PID=$!
+
+sleep 2
+curl -q "http://localhost:16421/power/control/reboot?hostname=test&port=1&delay=5" &> /dev/null
+sleep 10
+
+if [ $LOCAL ]
+then
+  # kill the running daemon after first test, helpful if running locally
+  kill $PDU_PID
+  sleep 5
+fi
+
+$PDUD_BINARY --loglevel=DEBUG --conf=share/pdudaemon.conf --drive --hostname drivetest --port 1 --request reboot
+
+if [ $LOCAL ]
+then
+  rm $PDUD_BINARY
+fi
+
+echo "#### Created output ####"
+cat $TMPFILE
+echo ""
+echo "#### Expected output ####"
+cat share/expected_output.txt
+echo ""
+
+diff -q -u share/expected_output.txt $TMPFILE
+exit $?
