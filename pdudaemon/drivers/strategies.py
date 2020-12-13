8ba2078b0968535f899b89f6a968611ee37bc378
Matt Hart <matt@mattface.org>
2020-12-13 15:49:29 +0000

    Add TPLink driver for TPLink WiFi strips and plugs
    Support TPLink KP303 and KP105 devices, available from Argos in the
    UK, but presumably also US/EU devices will work as well.
    Uses the json API, based on code from
    https://github.com/softScheck/tplink-smartplug

--- pdudaemon/drivers/strategies.py
+++ pdudaemon/drivers/strategies.py
@@ -62,3 +62,4 @@ from pdudaemon.drivers.snmp import SNMP
 from pdudaemon.drivers.energenieusb import EnerGenieUSB
 from pdudaemon.drivers.bcu import BCU
 from pdudaemon.drivers.vusbhid import VUSBHID
+from pdudaemon.drivers.tplink import TPLink
