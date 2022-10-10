6739a7dfe1f0d84e97b4bb7fa1930841823909d2
Julian Haller <julian.haller@philips.com>
2022-10-10 14:52:02 +0200

    Add driver for Gude Expert Power Control 1202 PDU
    The Gude Expert Power Control 1202 is a 4 port 230V PDU. The vendor
    offers 3 variants:
    
    - 1202-1: type F socket connectors (DE)
    - 1202-3: type J socket connectors (CH)
    - 1202-4: IEC C13 connectors
    
    The driver was tested with the 1202-1 variant, however the protocol
    looks identical for all 3 variants.
    
    Signed-off-by: Julian Haller <julian.haller@philips.com>

---
+++ pdudaemon/drivers/gude1202.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+
+#  Copyright (c) 2023 Koninklijke Philips N.V.
+#  Author Julian Haller <julian.haller@philips.com>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+
+import logging
+import pexpect
+from pdudaemon.drivers.driver import PDUDriver
+import os
+
+log = logging.getLogger("pdud.drivers." + os.path.basename(__file__))
+
+
+class Gude1202(PDUDriver):
+    connection = None
+
+    def __init__(self, hostname, settings):
+        self.hostname = hostname
+        self.settings = settings
+        telnetport = settings.get("telnetport", 23)
+
+        self.exec_string = "/usr/bin/telnet %s %d" % (hostname, telnetport)
+        super(Gude1202, self).__init__()
+
+    @classmethod
+    def accepts(cls, drivername):
+        if drivername == "gude1202":
+            return True
+        return False
+
+    def port_interaction(self, command, port_number):
+        log.debug("Running port_interaction")
+        self.get_connection()
+        log.debug("Attempting command: {} port: {}".format(command, port_number))
+        if command == "on":
+            self.connection.send("port %s state set 1\r" % port_number)
+        elif command == "off":
+            self.connection.send("port %s state set 0\r" % port_number)
+        else:
+            log.error("Unkown command")
+
+        self.connection.expect("OK.")
+
+    def get_connection(self):
+        log.debug("Connecting to Gude1202 PDU with: %s", self.exec_string)
+        self.connection = pexpect.spawn(self.exec_string)
+
+    def _cleanup(self):
+        if self.connection:
+            self._pdu_logout()
+            self.connection.close()
+
+    def _bombout(self):
+        if self.connection:
+            self.connection.close(force=True)
+
+    def _pdu_logout(self):
+        log.debug("Logging out")
+        self.connection.send("quit\r")
